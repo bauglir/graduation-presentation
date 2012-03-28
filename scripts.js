918af711af6edddec55fcd15f281e99470b6811d
Joris Kraak <jkraak@gnresound.com>
2012-03-28 16:07:26 +0200

    Made which version of MathJax is loaded conditional on location of the presentation

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@
 $(function () {
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.src  = "file/lib/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+
+  // Only use the locally cloned version of MathJax when running from localhost
+  if(window.location.hostname == "localhost") {
+    script.src  = "file/lib/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  } else {
+    script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  }
+
   document.getElementsByTagName("head")[0].appendChild(script);
 });
