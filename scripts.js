f4560284b85174fdc05ab9b6fb97ece9b951eec5
Joris Kraak <jkraak@gnresound.com>
2012-03-28 16:00:56 +0200

    Included MathJax as a local submodule

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-document.writeln('<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>');
+// Include MathJax
+$(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "file/lib/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  document.getElementsByTagName("head")[0].appendChild(script);
+});
