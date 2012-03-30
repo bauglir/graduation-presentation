e53b028f80f97c321f6bcb38f90960dd02ed17e6
Joris Kraak <jkraak@gnresound.com>
2012-03-30 17:50:56 +0200

    Updated loading of MathJax to start running on load

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 // Include MathJax
-$(function () {
+(function () {
   var script = document.createElement("script");
   script.type = "text/javascript";
 
   // Only use the locally cloned version of MathJax when running from localhost
   if(window.location.hostname == "localhost") {
-    script.src  = "file/lib/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+    script.src  = "/file/lib/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
   } else {
     script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
   }
 
   document.getElementsByTagName("head")[0].appendChild(script);
-});
+})();
