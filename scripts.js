3b3d1215dbe8c84b6fbfa397600b3731e8438a45
Joris Kraak <jkraak@gnresound.com>
2012-04-02 18:16:21 +0200

    Moved Algorithm Performance visualization related code into scripts.js

--- scripts.js
+++ scripts.js
@@ -21,6 +21,52 @@
   document.getElementsByTagName("head")[0].appendChild(script);
 })();
 
+function algorithmPerformanceForward(event) {
+  algorithmPerformanceMoveToPosition(0);
+  algorithmPerformanceMoveToPosition(3);
+  algorithmPerformanceMoveToPosition(4);
+
+  if(window.algorithmPerformanceStep == 0 || window.algorithmPerformanceStep == 3 || window.algorithmPerformanceStep == 4) {
+    if(window.algorithmPerformanceStep == 0) {
+      algorithmPerformanceZoom(0);
+    } else {
+      algorithmPerformanceZoom(window.algorithmPerformanceStep - 2);
+    }
+  } else {
+    var images = $('img', '.algorithm-performance');
+    $(images[0]).show();
+    $(images[3]).show();
+    $(images[4]).show();
+  }
+  window.algorithmPerformanceStep++;
+}
+
+function algorithmPerformanceZoom(id) {
+  var images = $('img', '.algorithm-performance');
+  $(images[0]).hide();
+  $(images[3]).hide();
+  $(images[4]).hide();
+
+  $(images[id]).css({ 'background-color' : 'white', 'left' : 112, 'width' : 800, 'z-index': 100 });
+  $(images[id]).show();
+}
+
+function algorithmPerformanceMoveToPosition(id) {
+  var bullets = $('li', '.algorithm-performance');
+  var left = [25, 0, 0, 350, 675];
+  imageStyling = { 'position': 'absolute', 'width': '350px', 'top': '125px' };
+  imageStyling.left = left[id] + 'px';
+  $('img', bullets[id]).css(imageStyling);
+}
+
+function algorithmPerformanceReset() {
+  algorithmPerformanceStep = 0;
+  var bullets = $('li', '.algorithm-performance');
+  $(bullets[0]).css('padding', '125px');
+  $(bullets[3]).css('padding', 0);
+  $(bullets[4]).css('padding', 0);
+}
+
 function drawLineGraph(target, data) {
   data  = [3,7,9,1,4,6,8,2,5];
   w     = 700;
