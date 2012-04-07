39501cf7f01dbbeb0f5cd6cec13f98ffe650dbcf
Joris Kraak <joris@majorfail.com>
2012-04-07 12:49:13 +0200

    Removed d3 graph line graph drawing function from scripts.js

--- scripts.js
+++ scripts.js
@@ -74,30 +74,6 @@ function conclusionsMarkup() {
   $(items[2]).css('color', 'orange');
 }
 
-function drawLineGraph(target, data) {
-  data  = [3,7,9,1,4,6,8,2,5];
-  w     = 700;
-  h     = 300;
-  max   = d3.max(data);
-
-  x  = d3.scale.linear().domain([0, data.length - 1]).range([0, w]);
-  y  = d3.scale.linear().domain([0, max]).range([h, 0]);
-
-  vis = d3.select('.conventional-results').append('svg:svg').attr('width', w).attr('height', h);
-  vis.selectAll('path.line').data([data, [5,2,8,6,4,1,9,7,3]]).enter().append("svg:path").attr("d", d3.svg.line().x(function (d,i) { return x(i) }).y(y));
-
-  ticks = vis.selectAll('.ticks').data(y.ticks(7)).enter().append('svg:g').attr('transform', function(d) { return "translate(0, #{y(d)})"; }).attr('class', 'tick');
-
-  ticks.append('svg:line').attr('y1', 0).attr('y2', 0).attr('x1', 0).attr('x2', w);
-  ticks.append('svg:text').text(function(d) { return d; }).attr('text-anchor', 'end').attr('dy', 2).attr('dx', -4);
-
-  vis.selectAll('.point').data(data).enter().append("svg:circle")
-    .attr("class", function(d, i) { if(d == max) { return 'point max'; } else { return 'point'; } })
-    .attr("cx", function(d, i) { return x(i) })
-    .attr("cy", function(d) { return y(d) })
-    .attr("r", function(d, i) { if(d == max) { return 6; } else { return 4; } })
-    .on('mouseover', function() { d3.select(this).attr('r', 8) })
-    .on('mouseout',  function() { d3.select(this).attr('r', 4) });
 }
 
 function modelBasedResultsReset() {
